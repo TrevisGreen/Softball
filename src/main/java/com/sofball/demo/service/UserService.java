a18d12d25ba78974831206c54eccd96e0bd285ea
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 15:51:26 -0600

    Created UserService interface

--- src/main/java/com/sofball/demo/service/UserService.java
+++ src/main/java/com/sofball/demo/service/UserService.java
@@ -1,4 +1,18 @@
 package com.sofball.demo.service;
 
+import com.sofball.demo.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface UserService {
+
+    public Page<User> search(String filter, PageRequest pageRequest);
+
+    public Page<User> list(PageRequest pageRequest);
+
+    public User create(User user);
+
+    public User get(Long userId);
+
+    public String delete(Long userId);
 }
