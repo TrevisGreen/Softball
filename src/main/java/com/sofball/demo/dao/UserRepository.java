17903cbea4226dc49db47bedc5dd1cdd1e897fb0
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 15:41:55 -0600

    created UserRepository interface

---
+++ src/main/java/com/sofball/demo/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sofball.demo.dao;
+
+
+import com.sofball.demo.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository extends PagingAndSortingRepository<User,Long> {
+
+    User findByUsernameIgnoreCase(String username);
+}
