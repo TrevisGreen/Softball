ee9078c7b5a0c79794c38debbcecd26a60f76bff
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 17:37:06 -0600

    Created TeamService interface

--- src/main/java/com/sofball/demo/service/TeamService.java
+++ src/main/java/com/sofball/demo/service/TeamService.java
@@ -1,4 +1,20 @@
 package com.sofball.demo.service;
 
+import com.sofball.demo.model.Team;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface TeamService {
+
+    public Page<Team> search(String filter, PageRequest pageRequest);
+
+    public Page<Team> list(PageRequest pageRequest);
+
+    public Team create(Team team);
+
+    public Team get(Integer teamId);
+
+    public String delete(Integer teamId);
+
+    public Iterable<Team> all();
 }
