aca85ee361258d21a7cb52efeb525a89128b7986
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 14:35:08 -0600

    Initial commit

---
+++ src/main/java/com/sofball/demo/SoftballApplication.java
@@ -0,0 +1,13 @@
+package com.sofball.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftballApplication.class, args);
+	}
+
+}
