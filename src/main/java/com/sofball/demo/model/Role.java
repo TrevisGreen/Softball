cfee8ddae9a1da2049d9e1ea2360867336d22795
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 16:07:58 -0600

    Created Role class

--- src/main/java/com/sofball/demo/model/Role.java
+++ src/main/java/com/sofball/demo/model/Role.java
@@ -1,4 +1,36 @@
 package com.sofball.demo.model;
 
-public class Role {
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"authority"})})
+public class Role implements Serializable, GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Size(min = 1, max = 20)
+    @Column(name = "authority", nullable = false, length = 20)
+    private String authority;
+
+    // default constructor
+    public Role(){
+    }
+
+    // Getters and Setters
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
