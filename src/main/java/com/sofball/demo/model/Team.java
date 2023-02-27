22a63f9904908882c9b1b1f3e2844e06d76451bb
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 17:29:23 -0600

    Created Team class

--- src/main/java/com/sofball/demo/model/Team.java
+++ src/main/java/com/sofball/demo/model/Team.java
@@ -1,4 +1,58 @@
 package com.sofball.demo.model;
 
-public class Team {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "teams")
+public class Team implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToOne(optional = false)
+    private Division division;
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Division getDivision() {
+        return division;
+    }
+
+    public void setDivision(Division division) {
+        this.division = division;
+    }
 }
