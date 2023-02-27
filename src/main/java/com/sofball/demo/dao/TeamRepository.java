0f2a640063907786769d94763ed133b97f539045
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 17:32:28 -0600

    Created TeamRepository interface

--- src/main/java/com/sofball/demo/dao/TeamRepository.java
+++ src/main/java/com/sofball/demo/dao/TeamRepository.java
@@ -1,4 +1,8 @@
 package com.sofball.demo.dao;
 
-public interface TeamRepository {
+import com.sofball.demo.model.Team;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface TeamRepository extends PagingAndSortingRepository<Team,Integer> {
+
 }
