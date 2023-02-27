68464d204930f6511f92a31443890ecf5025011d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 15:48:05 -0600

    Created BaseService class

--- src/main/java/com/sofball/demo/service/BaseService.java
+++ src/main/java/com/sofball/demo/service/BaseService.java
@@ -1,4 +1,9 @@
 package com.sofball.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class BaseService {
+
+    protected final transient Logger log = LoggerFactory.getLogger(getClass());
 }
