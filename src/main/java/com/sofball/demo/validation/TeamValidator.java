7bfee4d69d8b25d328cf61fdf217853cfe3dd0e8
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 17:59:11 -0600

    Created TeamValidator class

--- src/main/java/com/sofball/demo/validation/TeamValidator.java
+++ src/main/java/com/sofball/demo/validation/TeamValidator.java
@@ -1,4 +1,28 @@
 package com.sofball.demo.validation;
 
-public class TeamValidator {
+import com.sofball.demo.model.Team;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class TeamValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> type) {
+        return Team.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Team team = (Team) o;
+
+        if(StringUtils.isBlank(team.getName())) {
+            errors.rejectValue("name", "NotBlank.team.name");
+        }
+        if(team.getDivision() == null) {
+            errors.rejectValue("division", "NotNull.team.division");
+        }
+    }
 }
