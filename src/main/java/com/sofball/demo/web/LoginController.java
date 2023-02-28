18961f7882ba48bcdb09201de97cf3c535740f38
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 22:19:39 -0600

    Created LoginController class

--- src/main/java/com/sofball/demo/web/LoginController.java
+++ src/main/java/com/sofball/demo/web/LoginController.java
@@ -1,4 +1,20 @@
 package com.sofball.demo.web;
 
-public class LoginController {
+
+import com.sofball.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController extends BaseController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        log.debug("Showing login page");
+        User user = new User();
+        model.addAttribute("user", user);
+        return "/login/login";
+    }
 }
