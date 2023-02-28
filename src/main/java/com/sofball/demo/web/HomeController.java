8bb2f59bafd1ef4f867934295f3af9d1e4143f3d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 22:16:13 -0600

    Created HomeController class

--- src/main/java/com/sofball/demo/web/HomeController.java
+++ src/main/java/com/sofball/demo/web/HomeController.java
@@ -1,4 +1,16 @@
 package com.sofball.demo.web;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController extends BaseController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        log.debug("Home");
+        return "/home/index";
+    }
 }
