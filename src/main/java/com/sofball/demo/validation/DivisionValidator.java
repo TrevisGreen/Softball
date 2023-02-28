f7ba8b0853d52113706fea3ff00e7e7c4901ce97
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:24:51 -0600

    Created DivisionValidator class

--- src/main/java/com/sofball/demo/validation/DivisionValidator.java
+++ src/main/java/com/sofball/demo/validation/DivisionValidator.java
@@ -1,4 +1,24 @@
 package com.sofball.demo.validation;
 
-public class DivisionValidator {
+import com.sofball.demo.model.Division;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class DivisionValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> type) {
+        return Division.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Division division = (Division) o;
+        if(StringUtils.isBlank(division.getName())) {
+            errors.rejectValue("name", "NotBlank.division.name");
+        }
+    }
 }
