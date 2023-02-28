b3cc3bd9bcc087a0413a356e7be9196d9b492cd5
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 20:53:47 -0600

    Created Constants class

--- src/main/java/com/sofball/demo/utils/Constants.java
+++ src/main/java/com/sofball/demo/utils/Constants.java
@@ -1,4 +1,26 @@
 package com.sofball.demo.utils;
 
 public class Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "day";
+
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+    public static final String PAGE = "page";
+
+    public static final String SORT = "sort";
+
+    public static final String DIRECTION = "direction";
+
+    public static final String OPPOSITE_DIRECTION = "opposite";
+
+    public static final String PAGE_REQUEST = "pageRequest";
+
+    public static final String FILTER = "filter";
+
+    public static final String CURRENT_PAGE = "currentPage";
+
+    public static final String TOTAL_PAGES = "totalPages";
+
+    public static final String PAGES = "pages";
 }
