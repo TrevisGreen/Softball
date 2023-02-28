854639c5057825bec3e331de8e3ab26b70ea81b7
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:04:13 -0600

    Created Division class

--- src/main/java/com/sofball/demo/model/Division.java
+++ src/main/java/com/sofball/demo/model/Division.java
@@ -1,4 +1,58 @@
 package com.sofball.demo.model;
 
-public class Division {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "divisions")
+public class Division implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToOne(optional = false)
+    private Season season;
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
 }
