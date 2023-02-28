4c175ee475b194687dc8ec0b6b80881103ab726d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:09:29 -0600

    Created DivisionService interface

--- src/main/java/com/sofball/demo/service/DivisionService.java
+++ src/main/java/com/sofball/demo/service/DivisionService.java
@@ -1,4 +1,20 @@
 package com.sofball.demo.service;
 
+import com.sofball.demo.model.Division;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface DivisionService {
+
+    public Page<Division> search(String filter, PageRequest pageRequest);
+
+    public Page<Division> list(PageRequest pageRequest);
+
+    public Division create(Division division);
+
+    public Division get(Integer divisionId);
+
+    public String delete(Integer divisionId);
+
+    public Iterable<Division> all();
 }
