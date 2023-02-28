128e5b9b62b4a5e35ff989ef51ce4101c4e751c6
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 21:10:12 -0600

    Created ThymeleafConfig class

--- src/main/java/com/sofball/demo/config/ThymeleafConfig.java
+++ src/main/java/com/sofball/demo/config/ThymeleafConfig.java
@@ -1,4 +1,34 @@
 package com.sofball.demo.config;
 
+
+import com.sofball.demo.utils.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@Profile(Constants.SPRING_PROFILE_PRODUCTION)
 public class ThymeleafConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ThymeleafConfig.class);
+
+    @Value("${spring.thymeleaf.cache}")
+    private Boolean cache;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        log.info("Setting Thymeleaf Resolver Cache to {}", cache);
+        ITemplateResolver resolver = new ServletContextTemplateResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(cache);
+        return resolver;
+    }
 }
