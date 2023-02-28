289d11069ac9a8d91a9a0a1ba12d46dc53095c67
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:39:04 -0600

    Created SeasonValidator class

--- src/main/java/com/sofball/demo/validation/SeasonValidator.java
+++ src/main/java/com/sofball/demo/validation/SeasonValidator.java
@@ -1,4 +1,25 @@
 package com.sofball.demo.validation;
 
-public class SeasonValidator {
+
+import com.sofball.demo.model.Season;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class SeasonValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> type) {
+        return Season.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Season season = (Season) o;
+
+        if(season.getId() == null) {
+            errors.rejectValue("id", "NotNull.season.id");
+        }
+    }
 }
