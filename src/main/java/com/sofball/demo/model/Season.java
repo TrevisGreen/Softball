ef60ab0974c5d94ba6dfb76cf196ae1ebbea3491
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:28:44 -0600

    Created Season class

--- src/main/java/com/sofball/demo/model/Season.java
+++ src/main/java/com/sofball/demo/model/Season.java
@@ -1,4 +1,35 @@
 package com.sofball.demo.model;
 
-public class Season {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "seasons")
+public class Season implements Serializable {
+
+    @Id
+    private Integer id;
+
+    // Default constructor
+    public Season() {
+    }
+
+    // Param constructor
+    public Season(Integer id) {
+        this.id = id;
+    }
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
