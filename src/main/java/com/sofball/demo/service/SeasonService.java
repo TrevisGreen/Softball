314d487c3807bcdedf557f741c54ee30158a017c
TrevisGreen <Trevis.Green@gmail.com>
2023-02-27 18:32:44 -0600

    Created SeasonService interface

--- src/main/java/com/sofball/demo/service/SeasonService.java
+++ src/main/java/com/sofball/demo/service/SeasonService.java
@@ -1,4 +1,20 @@
 package com.sofball.demo.service;
 
+import com.sofball.demo.model.Season;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface SeasonService {
+
+    public Page<Season> search(String filter, PageRequest pageRequest);
+
+    public Page<Season> list(PageRequest pageRequest);
+
+    public void create(Season season);
+
+    public Season get(Integer seasonId);
+
+    public void delete(Integer seasonId);
+
+    public Iterable<Season> all();
 }
